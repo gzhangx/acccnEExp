ff4fab4c49585dfb8ff16d0eab2a7f15894f4ae0
Gang Zhang <gzhangx@hotmail.com>
2022-04-19 16:40:49 -0400

    added

--- localMissionExpenseRecorder/index.ts
+++ localMissionExpenseRecorder/index.ts
@@ -9,7 +9,24 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     
+    function errorRsp(error: string) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                error,
+            },
+        };
+    }
     const reqBody = req.body;
+    if (!reqBody.amount) {
+        return errorRsp('no amount');
+    }    
+    if (!reqBody.payeeName) {
+        return errorRsp('no payeeName');
+    }
+    if (!reqBody.reimbursementCat) {
+        return errorRsp('no reimbursementCat');
+    }
     const res = await submitFile({
         amount: reqBody.amount,
         description: reqBody.description,
